ada9a6906b9d4b6c7e2fb90d2537745eda78fbad
Yurii Malikov <malikov.yurii.alex@gmail.com>
2017-06-08 15:55:38 +0300

    add filter remove interceptor

---
+++ src/main/java/com/malikov/ticketsystem/web/filter/AddFullNameToRequestFilter.java
@@ -0,0 +1,25 @@
+package com.malikov.ticketsystem.web.filter;
+
+import com.malikov.ticketsystem.AuthorizedUser;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Yurii Malikov
+ */
+public class AddFullNameToRequestFilter extends GenericFilterBean {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+            AuthorizedUser authorizedUser = AuthorizedUser.safeGet();
+            if (authorizedUser != null) {
+                request.setAttribute("userFullName", authorizedUser.getFullName());
+            }
+        chain.doFilter(request, response);
+    }
+}
